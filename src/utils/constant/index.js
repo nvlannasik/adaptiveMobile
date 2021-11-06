153275a51537a748098a1b51bf15e5137abd8e53
nvlannasik <annasikn@yahoo.com>
2021-11-06 18:43:26 +0700

    menambahkan utilitis responsive

---
+++ src/utils/constant/index.js
@@ -0,0 +1,2 @@
+export const heightMobileUI = 812;
+export const widthMobileUI = 375;
---
+++ src/utils/utils/index.js
@@ -0,0 +1,10 @@
+import {Dimensions} from 'react-native';
+import {widthMobileUI} from '../constant';
+
+export const responsiveWidht = width => {
+  return (Dimensions.get('window').width * width) / widthMobileUI;
+};
+
+export const responsiveHeight = height => {
+  return (Dimensions.get('window').height * height) / heightMobileUI;
+};
