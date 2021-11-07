f584e5d3c9dc299e5ad6fa1a1a6937587eb32375
nvlannasik <annasikn@yahoo.com>
2021-11-07 15:14:49 +0700

    edit component slider

--- src/components/besar/BannerSlider/index.js
+++ src/components/besar/BannerSlider/index.js
@@ -3,12 +3,14 @@ import {StyleSheet, View, Text, Image} from 'react-native';
 import {SliderBox} from 'react-native-image-slider-box';
 import {Banner1, Banner2, Banner3, Banner4} from '../../../assets/index';
 import {responsiveHeight, responsiveWidht} from '../../../utils';
+import Dots from 'react-native-dots-pagination';
 
 export default class BannerSlider extends Component {
   constructor(props) {
     super(props);
     this.state = {
       images: [Banner1, Banner2, Banner3, Banner4],
+      active: 0,
     };
   }
 
@@ -21,6 +23,8 @@ export default class BannerSlider extends Component {
           circleLoop
           sliderBoxHeight={responsiveHeight(189)}
           ImageComponentStyle={styles.slider}
+          dotColor={'#326CE5'}
+          imageLoadingColor={'#326CE5'}
         />
       </View>
     );
@@ -29,7 +33,7 @@ export default class BannerSlider extends Component {
 
 const styles = StyleSheet.create({
   dotstyle: {
-    marginTop: 10,
+    backgroundColor: 'blue',
   },
   slider: {
     borderRadius: 10,
